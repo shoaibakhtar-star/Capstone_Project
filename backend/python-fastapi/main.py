c8cb9c574985fa6c47c0da827e9634b26e5c9629
Shoaib Akhtar <shoaib.akhtar@tudip.com>
2026-04-30 19:23:02 +0530

    asdf

--- backend/python-fastapi/main.py
+++ backend/python-fastapi/main.py
@@ -91,9 +91,9 @@ def health():
     try:
         db = get_db()
         db.close()
-        return {"status": "healthy Fastapi"}
+        return {"status": "Fastapi is Healthy"}
     except:
-        return JSONResponse(content={"status": "unhealthy Fastapi"}, status_code=503)
+        return JSONResponse(content={"status": "Fastapi is Unhealthy"}, status_code=503)
 
 
 @app.post("/auth/register")
